767d28cf07cd8c94cfc074c135ea3f52bb10186e
jdlace <jdlace@icloud.com>
2016-04-21 11:10:49 -0400

    added print function

--- Homeowner/ItemsViewController.swift
+++ Homeowner/ItemsViewController.swift
@@ -26,6 +26,8 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        print("Hello, World")
     }
     
     @IBAction func addNewItem(sender: AnyObject) {
